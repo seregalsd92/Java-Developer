57a345149767ed4b263f72edf6ec72bc7a040aad
s.d.lapin <s.d.lapin@tinkoff.ru>
2024-02-25 21:10:33 +0300

    sum and product

---
+++ hw_4.1_Arithmetic/src/Arithmetic.java
@@ -0,0 +1,18 @@
+public class Arithmetic {
+    private int x;
+    private int y;
+
+    public Arithmetic (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int sum () {
+        return x + y;
+    }
+
+    public int product() {
+        return x * y;
+    }
+
+}
---
+++ hw_4.1_Arithmetic/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Arithmetic first = new Arithmetic(4,5);
+
+        System.out.println(first.sum());
+        System.out.println(first.product());
+    }
+}
